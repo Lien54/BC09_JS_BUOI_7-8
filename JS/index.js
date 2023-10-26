62d5cd540db6cb1041ef8652feea066e00eb46fe
Liên Hồ <holien227305@gmail.com>
2023-10-26 11:20:04 +0700

    update bai tap

--- JS/index.js
+++ JS/index.js
@@ -58,18 +58,44 @@ document.getElementById("btnketQuaSoDuongNhoNhat").addEventListener("click", fun
 
     // Câu 5: Tìm số chẵn cuối cùng
 document.getElementById("btnSoChan").addEventListener("click", function () {
-    var soChan= Infinity;
-    for (var i = 0; i < arrSoN.length; i++) {
-      nb = arrSoN[i];
-      if (so < soMinDuong) {
-        soMinDuong = so;
-      }
+  var soChanCuoiCung = -1;
+  for (i = arrSoN.length - 1; i >= 0 && soChanCuoiCung == -1; i--) {
+    if (arrSoN[i] > 0 && arrSoN[i] % 2 == 0) {
+      soChanCuoiCung = arrSoN[i];
+      console.log(soChanCuoiCung);
+      document.getElementById(
+        "ketQuaSoChan"
+      ).innerHTML = `Số chẵn cuối cùng ${soChanCuoiCung}`;
+    } else {
+      document.getElementById("ketQuaSoChan").innerHTML = -1;
     }
-    document.getElementById("inSoChan").innerHTML = soMinDuong;
+  }
 });
 
     // Câu 6: Đổi chỗ
+document.getElementById("btnDoiCho").addEventListener("click", function () {
+  var So1 = document.getElementById("So1").value * 1;
+  var So2 = document.getElementById("So2").value * 1;
 
+  if (
+    So1 >= 0 &&
+    So1 < arrSoN.length &&
+    So2 >= 0 &&
+    So2 < arrSoN.length
+  ) {
+    // console.log(arrSoN[So1]);
+    // console.log(arrSoN[So2]);
+    var tamThoi = arrSoN[So1];
+    arrSoN[So1] = arrSoN[So2];
+    arrSoN[So2] = tamThoi;
+    // console.log(arrSoNhap);
+    document.getElementById(
+      "ketQuaDoiCho"
+    ).innerHTML = `Mảng dãy số sau khi đã đổi chỗ vị trí 2 giá trị là: ${arrSoN}`;
+  } else {
+    alert("Yêu cầu nhập vào vị trí hợp lệ");
+  }
+});
 
     // Câu 7: Sắp xếp tăng dần
 document.getElementById('btnSapxep').addEventListener('click', function () {
@@ -83,18 +109,59 @@ document.getElementById('btnSapxep').addEventListener('click', function () {
 })
 
     // Câu 8: Tìm số nguyên tố đầu tiên
-document.getElementById('btnSoNguyenTo').onclick = function () {
-    for (i = 2; i <= a - 1; i++) {
-        if (a % i == 0) {
-            console.log("Not Prime");
-            return;
-        }
+document.getElementById("btnSoNguyenTo").addEventListener("click", function () {
+  function checkSo(nb) {
+    if (nb <= 1) return false;
+    for (var z = 2; z <= Math.sqrt(nb); z++) {
+      if (nb % z === 0) return false;
     }
-    console.log("Prime");
-    return;
-    document.getElementById("inSoChan").innerHTML = soMinDuong;
-}
+    return true;
+  }
+  var soNguyenToDauTien = "";
+  for (var i = 0; i < arrSoN.length; i++) {
+    if (checkSo(arrSoN[i])) {
+      soNguyenToDauTien += arrSoN[i];
+      document.getElementById(
+        "ketQuaSoNguyenTo"
+      ).innerHTML = `Số ${soNguyenToDauTien} là số nguyên tố đầu tiên trong mảng dãy số`;
+      return;
+    } else {
+      document.getElementById("ketQuaSoNguyenTo").innerHTML = -1;
+    }
+  }
+});
     // Câu 9: Đếm số nguyên
+document.getElementById("btnSoNguyen").addEventListener("click", function () {
+  var arrNhapSoThuc = [];
+  var demSoNguyen = 0;
+  for (a = 0; a < arrNhapSoThuc.length; a++) {
+    if (arrNhapSoThuc[a] % 1 == 0) {
+      demSoNguyen++;
+    }
+  }
+  document.getElementById(
+    "ketQuaDemSoNguyen"
+  ).innerHTML = `Kết quả đếm được ${demSoNguyen} số nguyên`;
+});
 
 
     // Câu 10: So sánh số lượng số âm và dương
+document.getElementById("btnSoAmDuong").addEventListener("click", function () {
+  var soDuongSoSanh = 0;
+  var soAmSoSanh = 0;
+
+  for (i = 0; i < arrSoN.length; i++) {
+    if (arrSoN[i] < 0) {
+      soAmSoSanh++;
+    } else if (arrSoN[i] > 0) {
+      soDuongSoSanh++;
+    }
+  }
+  if (soDuongSoSanh > soAmSoSanh) {
+    document.getElementById("ketQuaSoAmDuong").innerHTML = "số Dương > số Âm";
+  } else if (soDuongSoSanh < soAmSoSanh) {
+    document.getElementById("ketQuaSoAmDuong").innerHTML = "Số Dương < số Âm";
+  } else {
+    document.getElementById("ketQuaSoAmDuong").innerHTML = "số Dương = số Âm";
+  }
+});
